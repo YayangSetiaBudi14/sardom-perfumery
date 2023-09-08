afdced814586b287bbeba405840793da830fd310
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-09-09 06:34:33 +0700

    add Cart and Carts Repository

--- spring-boot/src/main/java/com/mahasiswa/repository/CartRepository.java
+++ spring-boot/src/main/java/com/mahasiswa/repository/CartRepository.java
@@ -1,8 +1,8 @@
 package com.mahasiswa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 
 import com.mahasiswa.model.Cart;
 
