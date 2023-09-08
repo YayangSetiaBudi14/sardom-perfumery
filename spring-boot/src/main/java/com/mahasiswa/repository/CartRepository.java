cccfada6ca55fa32875e4054a36d72e46324a905
YayangSB <yayang.setia.tif420@polban.ac.id>
2023-09-09 06:05:09 +0700

    Add Database Carts and Cart

---
+++ spring-boot/src/main/java/com/mahasiswa/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.mahasiswa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.mahasiswa.model.Cart;
+
+public interface CartRepository extends JpaRepository<Cart, String> {
+
+}
